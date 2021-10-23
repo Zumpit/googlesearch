8a8b6149c1c4b5637189a5198b3342c46b8c000f
imadhrit <maharajrabel@gmail.com>
2021-10-23 17:24:26 +0530

    limit

--- limitrate.go
+++ limitrate.go
@@ -6,8 +6,7 @@ import (
 )
 
 // rate limiting request otherwise google started to block you IP 
-
-errBlocked := errors.New("google block")
+var ErrBlocked = errors.New("google block")
 
 //Use golang rate function to limit the requets send by the client in the given time frame
-var RateLimit - rate.NewLimiter(rate.Inf, 0)
+var RateLimit = rate.NewLimiter(rate.Inf, 0)
